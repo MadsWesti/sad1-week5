2c557ac271abec048df2592b9577d41447f518f6
madswesti <madswesti@gmail.com>
2014-10-01 10:23:56 +0200

    debugging

--- max_flow_min_cut.py
+++ max_flow_min_cut.py
@@ -1,5 +1,6 @@
-import sys
+import sys, math
 from Queue import *
+from operator import attrgetter
 
 class Node:
     def __init__(self, id, name):
@@ -8,39 +9,80 @@ class Node:
 
 
 class Edge:
-    def __init__(self, v, w, c):
+    def __init__(self, u, v, capacity, reverse = None):
+        self.u = u
         self.v = v
-        self.w = w
-        self.c = c
-        self.f = 0
+        self.capacity = capacity
+        self.flow = 0
+        self.residual_capacity = self.capacity - self.flow
 
-    def add_flow(self, f):
-        self.f += f
+        if not reverse:
+            self.reverse = Edge(v, u, capacity, self)
+        else:
+            self.reverse = reverse
+
+    def add_flow(self, flow):
+        self.flow += flow
+        self.reverse.flow -= flow
+        self.residual_capacity = self.capacity - self.flow
+        self.reverse.residual_capacity = self.reverse.capacity - self.reverse.flow
 
     def __repr__(self):
-        return str(self.v.name) + " -- " + str(self.f) + "/" + str(self.c) + " -> " + str(self.w.name)
+        return str(self.u.name) + "-->" + str(self.v.name) + " - " + str(self.flow) + "/" + str(self.capacity)
 
 
 class Graph:
-    def __init__(self, N, E):
+    def __init__(self, N, M):
         self.nodes = []
-        self.edges = []
-        self.adjacency_list = []
+        self.adjacency_list = {}
 
-        for i in range(0, N):
-            self.adjacency_list.append([])
 
-    def addEdge(self, v_id,  w_id, c):
+    def add_edge(self, u_id,  v_id, capacity):
+        u = self.nodes[u_id]
         v = self.nodes[v_id]
-        w = self.nodes[w_id]
-        e = Edge(v, w, c)
+        e = Edge(u, v, capacity)
 
-        self.edges.append(e)
-        self.adjacency_list[v_id].append(e)
+        self.adjacency_list[u].append(e)
+        self.adjacency_list[v].append(e.reverse)
 
 
-    def addNode(self, v):
-        self.nodes.append(v)
+    def add_node(self, u):
+        self.nodes.append(u)
+        self.adjacency_list[u] = []
+
+    def max_flow(self):
+        f = 0
+        while True:
+            P = self.find_path(self.nodes[0], self.nodes[-1], [])
+            if not P:
+                break
+            self.augment(f, P)
+
+        for edge in self.adjacency_list[self.nodes[0]]:
+            f += edge.flow
+
+        return f
+
+    def augment(self, f, P):
+        b = self.bottleneck(P)
+        for edge in P:
+            edge.add_flow(b)
+
+
+    def bottleneck(self, P):
+        min_residual_capacity = min(P, key = attrgetter('residual_capacity')).residual_capacity
+        return min_residual_capacity
+
+
+    def find_path(self, source, sink, path):
+        if source == sink:
+            return path
+
+        for edge in self.adjacency_list[source]:
+            if edge.residual_capacity > 0 and edge not in path:
+                result = self.find_path(edge.v, sink, path + [edge]) 
+                if result != None:
+                    return result
 
 
 def parse_data():
@@ -48,38 +90,22 @@ def parse_data():
     N = int(data[0])
     nodes = data[1:N+1]
     M = int(data[N+1])
+    edges = data[N+2:]
     
     g = Graph(N, M)
-    for i, name in enumerate(nodes):
-        g.addNode(Node(i, name))
 
-    for d in data[N+2:]:
-        d = d.split()
-        v_id = int(d[0])
-        w_id = int(d[1])
-        c = int(d[2])
-        g.addEdge(v_id, w_id, c)
+    for i, name in enumerate(nodes):
+        g.add_node(Node(i, name))
+
+    for e in edges:
+        e = e.split()
+        u_id = int(e[0])
+        v_id = int(e[1])
+        capacity = int(e[2])
+        g.add_edge(u_id, v_id, capacity)
 
     return g
 
 
-def bfs(graph):
-    source_egdes = graph.adjacency_list[0]
-    frontier = Queue()
-    frontier.put(source_egdes)
-    while not frontier.empty():
-        current_edge = frontier.get()
-        print current_edge
-        current_node = current_edge.w
-        frontier.put(graph.adjacency_list[current_node.id])
-        #print current_edge
-        
-
-
 g = parse_data()
-bfs(g)
-
-
-for e in g.edges:
-    pass
-    #print e
+print g.max_flow()
